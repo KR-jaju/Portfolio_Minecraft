55a13cf1c4f28afa93bdb49a8f6157b3ba8c5fb5
wnwoals0127 <wnwoals0127@gmail.com>
2024-08-02 16:01:43 +0900

    Buffer class is now non-copyable

--- scop/scop/Buffer.h
+++ scop/scop/Buffer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+
 template<typename Data>
 class Buffer
 {
@@ -8,21 +9,19 @@ public:
 		ComPtr<ID3D11Device> device,
 		Data const* data_array,
 		uint32 count,
-		UINT const& bind_flag
+		UINT bind_flag
 	)
 		: device(device)
 	{
 		this->stride = sizeof(Data);
 		this->count = count;
 		
-		D3D11_BUFFER_DESC desc;
-		ZeroMemory(&desc, sizeof(desc));
+		D3D11_BUFFER_DESC desc = {};
 		desc.ByteWidth = this->stride * this->count;
 		desc.Usage = D3D11_USAGE_IMMUTABLE;
 		desc.BindFlags = bind_flag;
 
-		D3D11_SUBRESOURCE_DATA data;
-		ZeroMemory(&data, sizeof(data));
+		D3D11_SUBRESOURCE_DATA data = {};
 		data.pSysMem = data_array;
 		HRESULT hr = this->device->CreateBuffer(
 			&desc,
@@ -31,12 +30,13 @@ public:
 		);
 		CHECK(hr);
 	}
+	Buffer(Buffer const&) = delete;
 	~Buffer() {};
+	Buffer& operator=(Buffer const&) = delete;
 	ComPtr<ID3D11Buffer> getComPtr() const { return this->buffer; }
 	uint32 getStride() const { return this->stride; }
 	uint32 getOffset() const { return this->offset; }
 	uint32 getCount() const { return this->count; }
-
 private:
 	ComPtr<ID3D11Device> device;
 	ComPtr<ID3D11Buffer> buffer;
