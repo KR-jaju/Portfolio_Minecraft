eb5af1624b8a16e5db93956a904867a790b3dde4
wnwoals0127 <wnwoals0127@gmail.com>
2024-08-05 15:42:22 +0900

    Texture is now non-copyable

--- scop/scop/Texture.h
+++ scop/scop/Texture.h
@@ -6,7 +6,9 @@ public:
 		ComPtr<ID3D11Device> device,
 		wstring const& path
 	);
+	Texture(Texture const&) = delete;
 	~Texture();
+	Texture& operator=(Texture const&) = delete;
 	ComPtr<ID3D11ShaderResourceView> getComPtr() const;
 private:
 	ComPtr<ID3D11Device> device;
