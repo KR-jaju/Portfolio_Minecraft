d50e63219262d5df8e48679cf71b8acf483dd673
wnwoals0127 <wnwoals0127@gmail.com>
2024-08-17 17:29:33 +0900

    Feat: DSV with shader resource support

--- scop/scop/DepthStencilView.cpp
+++ scop/scop/DepthStencilView.cpp
@@ -8,11 +8,9 @@ DepthStencilView::DepthStencilView(Context const& context, DepthStencilTexture c
 {
 	ID3D11Texture2D* const	internal_texture = texture.getInternalResource();
 	ID3D11DepthStencilView** const	view = this->view.GetAddressOf();
-	D3D11_TEXTURE2D_DESC				texture_desc = {};
 	D3D11_DEPTH_STENCIL_VIEW_DESC		desc = {};
 
-	internal_texture->GetDesc(&texture_desc);
-	desc.Format = texture_desc.Format;
+	desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
 	desc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
 	desc.Texture2D.MipSlice = 0;
 
